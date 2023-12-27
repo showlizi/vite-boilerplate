07f40e5b556586020af90392f7beba103cf880ee
showlizi <103407187+showlizi@users.noreply.github.com>
2023-12-28 03:05:59 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 // import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import twaLogo from './assets/tapps.png'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 import WebApp from '@twa-dev/sdk'
 
 function App() {
   // const [count, setCount] = useState(0)
+  console.log(WebApp)
 
   return (
     <>
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is `)}>
-            Show Alert
-        </button>
+      <div>tg lizi 测试</div>
+      
+      <div>
+        {
+          WebApp?.WebAppUser
+        }
       </div>
+
+      <div>
+        {
+          WebApp?.initData
+        }
+      </div>
+
+      <button onClick={() => WebApp.showAlert(JSON.stringify(WebApp))}>
+        Show Alert
+      </button>
     </>
   )
 }
