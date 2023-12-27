7f17aacb60683f6d6be8c916cdd5ef334f0caae2
showlizi <103407187+showlizi@users.noreply.github.com>
2023-12-28 03:03:07 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import twaLogo from './assets/tapps.png'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import WebApp from '@twa-dev/sdk'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+        <button onClick={() => WebApp.showAlert(`Hello World! Current count is `)}>
             Show Alert
         </button>
       </div>
